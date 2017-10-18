497bda0378a842e5946e17c9917112d851a6e368
zunpiau <1330456228@qq.com>
2017-10-18 21:21:00 +0800

    优化数据库结构

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,14 +7,14 @@ CREATE TABLE user
 (
   id       INT AUTO_INCREMENT
     PRIMARY KEY,
-  uid      INT UNIQUE                NOT NULL,
-  name     VARCHAR(16) UNIQUE        NOT NULL,
-  password VARCHAR(128)              NOT NULL,
-  email    VARCHAR(64) UNIQUE        NOT NULL,
-  status   TINYINT                   NOT NULL,
-  UNIQUE (name, email, uid),
-  INDEX (name),
-  INDEX (email)
+  uid      INT         NOT NULL,
+  name     VARCHAR(16) NOT NULL,
+  password VARCHAR(16) NOT NULL,
+  email    VARCHAR(32) NOT NULL,
+  status   TINYINT     NOT NULL,
+  UNIQUE uk_uid(uid),
+  UNIQUE uk_name(name),
+  UNIQUE uk_email(email)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET utf8mb4
@@ -35,8 +35,8 @@ CREATE TABLE wordbooks
   sentence VARCHAR(4096) DEFAULT NULL,
   note     VARCHAR(1024) DEFAULT NULL,
   category VARCHAR(32)   DEFAULT NULL,
-  INDEX (uid),
-  INDEX (uid, word)
+  INDEX idx_uid(uid),
+  UNIQUE uk_uid_word(uid, word)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET utf8mb4
@@ -46,12 +46,12 @@ CREATE TABLE token
 (
   id   INT AUTO_INCREMENT
     PRIMARY KEY,
-  uid  INT NOT NULL,
-  time LONG,
-  sign VARCHAR(64),
-  used BOOL,
-  INDEX (uid),
-  INDEX (uid, sign, used)
+  uid  INT         NOT NULL,
+  time LONG        NOT NULL,
+  sign VARCHAR(64) NOT NULL,
+  used BOOL        NOT NULL,
+  INDEX idx_uid(uid),
+  INDEX idx_uid_sign_used(uid, sign, used)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET utf8mb4
@@ -62,11 +62,10 @@ CREATE TABLE feedback
   id      INT AUTO_INCREMENT
     PRIMARY KEY,
   data    TIMESTAMP,
-  content VARCHAR(512),
+  content VARCHAR(1024),
   contact VARCHAR(64),
   ua      VARCHAR(512)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET utf8mb4
   COLLATE utf8mb4_unicode_ci;
-
