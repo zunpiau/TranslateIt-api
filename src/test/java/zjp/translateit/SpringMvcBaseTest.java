d39301461a8cf67361215d2a4ce3df69a8fb5249
zunpiau <1330456228@qq.com>
2018-03-12 17:52:25 +0800

    :bug: Fixing 0a5083a13ee1a73a032c6289ff61359d535cc323.

--- src/test/java/zjp/translateit/SpringMvcBaseTest.java
+++ src/test/java/zjp/translateit/SpringMvcBaseTest.java
@@ -6,10 +6,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import zjp.translateit.config.RootConfig;
+import zjp.translateit.web.WebConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(classes = RootConfig.class)
+@ContextConfiguration(classes = {RootConfig.class, WebConfig.class})
 @ActiveProfiles("dev")
 public abstract class SpringMvcBaseTest {
 
