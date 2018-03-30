8742fa0719a9072f9210626b5de70db90c08fcad
zunpiau <1330456228@qq.com>
2018-03-30 20:44:15 +0800

    :recycle: 去除 Cookie.

--- src/main/java/zjp/translateit/web/controller/ManageController.java
+++ src/main/java/zjp/translateit/web/controller/ManageController.java
@@ -2,16 +2,11 @@ package zjp.translateit.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 import zjp.translateit.service.ManageService;
 import zjp.translateit.web.response.Response;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
-import static zjp.translateit.Constant.AUTH_TOKEN;
 
 @Controller
 @RequestMapping("/manage")
@@ -31,26 +26,13 @@ public class ManageController {
     }
 
     @RequestMapping(value = "/token", method = RequestMethod.POST)
-    public RedirectView login(@RequestParam String name, @RequestParam String password, HttpServletResponse response) {
+    public ResponseEntity login(@RequestParam String name, @RequestParam String password) {
         String tokenString = service.login(name, password);
         if (tokenString == null) {
-            throw new LoginException();
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         } else {
-            Cookie cookie = new Cookie(AUTH_TOKEN, tokenString);
-            cookie.setPath("/view");
-            cookie.setHttpOnly(false);
-            response.addCookie(cookie);
-            return new RedirectView("/view/manage.html");
+            return ResponseEntity.ok(tokenString);
         }
-    }
-
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    @ExceptionHandler(LoginException.class)
-    public void forbidden() {
-    }
-
-    private static class LoginException extends RuntimeException {
-
     }
 
 }
