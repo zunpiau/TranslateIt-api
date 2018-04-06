e8acfbd1788fc68959a062363ff203f387b3d672
zunpiau <1330456228@qq.com>
2018-04-06 21:40:07 +0800

    :sparkles: 500 页面.

--- src/main/java/zjp/translateit/web/controller/GlobalController.java
+++ src/main/java/zjp/translateit/web/controller/GlobalController.java
@@ -2,6 +2,7 @@ package zjp.translateit.web.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -12,6 +13,13 @@ public class GlobalController {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String notFound() {
         return "404";
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String serverError() {
+        return "500";
     }
 
 }
