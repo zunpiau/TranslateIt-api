6596e10d53460b73d4ea219c4835f76b5f40b5aa
zunpiau <1330456228@qq.com>
2018-05-01 03:04:32 +0800

    :recycle: Refactoring code.

--- src/main/java/zjp/translateit/config/LogbackConfig.java
+++ src/main/java/zjp/translateit/config/LogbackConfig.java
@@ -2,14 +2,20 @@ package zjp.translateit.config;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.ConsoleAppender;
+import ch.qos.logback.core.rolling.RollingFileAppender;
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
 import ch.qos.logback.ext.spring.ApplicationContextHolder;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@Import({LogbackConfigProd.class, LogbackConfigDev.class})
+@PropertySource(value = "classpath:application.properties")
 public class LogbackConfig {
 
     @Bean
@@ -28,5 +34,34 @@ public class LogbackConfig {
         encoder.setContext(ctx);
         encoder.setPattern("%d{yy-MM-dd HH:mm:ss.SSS} [%-5level] [%thread] %logger - %msg%n");
         return encoder;
+    }
+
+    @Profile("dev")
+    @Bean(initMethod = "start", destroyMethod = "stop", name = "appender")
+    public static ConsoleAppender<ILoggingEvent> consoleAppender(LoggerContext ctx, PatternLayoutEncoder encoder) {
+        ConsoleAppender<ILoggingEvent> appender = new ConsoleAppender<>();
+        appender.setContext(ctx);
+        appender.setEncoder(encoder);
+        return appender;
+    }
+
+    @Profile("!dev")
+    @Bean(initMethod = "start", destroyMethod = "stop", name = "appender")
+    public static RollingFileAppender<ILoggingEvent> fileAppender(LoggerContext ctx,
+            PatternLayoutEncoder encoder,
+            @Value("${log.history}") int history,
+            @Value("${log.fileNamePattern}") String parretn) {
+        RollingFileAppender<ILoggingEvent> appender = new RollingFileAppender<>();
+        appender.setContext(ctx);
+        TimeBasedRollingPolicy rollingPolicy = new TimeBasedRollingPolicy();
+        rollingPolicy.setMaxHistory(history);
+        rollingPolicy.setParent(appender);
+        rollingPolicy.setContext(ctx);
+        rollingPolicy.setCleanHistoryOnStart(true);
+        rollingPolicy.setFileNamePattern(parretn);
+        rollingPolicy.start();
+        appender.setRollingPolicy(rollingPolicy);
+        appender.setEncoder(encoder);
+        return appender;
     }
 }
