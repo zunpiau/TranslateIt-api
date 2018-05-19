b3c33b44d51a27c7b61c9218d62c83543e2d9bd6
zunpiau <zunpiau@qq.com>
2018-05-19 21:04:25 +0800

    :wheelchair: Improving accessibility.

--- src/main/java/zjp/translateit/service/EmailService.java
+++ src/main/java/zjp/translateit/service/EmailService.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import zjp.translateit.web.exception.EmailSendException;
 import zjp.translateit.web.request.FeedbackRequest;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -26,7 +27,8 @@ import java.text.MessageFormat;
 public class EmailService {
 
     private final IAcsClient client;
-    private final String verifyTemplate;
+    private final String verifyHtmlTemplate;
+    private final String verifyTextTemplate;
     private final Logger logger;
     private String feedbackTemplate;
     @Value("${ali.emailAccount}")
@@ -40,18 +42,23 @@ public class EmailService {
             @Value("${ali.accessSecret}") String secret) throws IOException, ClientException {
         client = new DefaultAcsClient(DefaultProfile.getProfile("cn-hangzhou", keyID, secret));
         DefaultProfile.addEndpoint("dm.aliyuncs.com", "cn-hangzhou", "Dm", "dm.aliyuncs.com");
-        verifyTemplate = new String(Files.readAllBytes(new ClassPathResource("email-template.html").getFile().toPath()),
+        verifyHtmlTemplate = new String(Files.readAllBytes(new ClassPathResource("email-template.html").getFile().toPath()),
                 StandardCharsets.UTF_8);
         feedbackTemplate = new String(Files.readAllBytes(new ClassPathResource("email-feedback.html").getFile().toPath()),
+                StandardCharsets.UTF_8);
+        verifyTextTemplate = new String(Files.readAllBytes(new ClassPathResource("email-template.txt").getFile().toPath()),
                 StandardCharsets.UTF_8);
         logger = LoggerFactory.getLogger(getClass());
     }
 
     public void sendVerifyEmail(String mailTo, String verifyCode) {
-        sendEmail(mailTo, verifyEmailSubject, MessageFormat.format(verifyTemplate, verifyCode));
+        sendEmail(mailTo,
+                verifyEmailSubject,
+                MessageFormat.format(verifyTextTemplate, verifyCode),
+                MessageFormat.format(verifyHtmlTemplate, verifyCode));
     }
 
-    private void sendEmail(String mailTo, String subject, String content) {
+    private void sendEmail(String mailTo, String subject, @Nullable String textBody, @Nullable String htmlBody) {
         SingleSendMailRequest request = new SingleSendMailRequest();
         request.setAccountName(aliAccount);
         request.setFromAlias("TranslateIt");
@@ -59,7 +66,12 @@ public class EmailService {
         request.setReplyToAddress(true);
         request.setToAddress(mailTo);
         request.setSubject(subject);
-        request.setHtmlBody(content);
+        if (textBody != null) {
+            request.setTextBody(textBody);
+        }
+        if (htmlBody != null) {
+            request.setHtmlBody(htmlBody);
+        }
         try {
             HttpResponse response = client.doAction(request, true, 2);
             logger.debug("send email to [{}] status [{}]", mailTo, response.getStatus());
@@ -74,7 +86,7 @@ public class EmailService {
 
     @Async
     public void sendFeedbackEmail(FeedbackRequest request, String ua) {
-        sendEmail(emailReply, "Feedback", MessageFormat.format(feedbackTemplate,
+        sendEmail(emailReply, "Feedback", null, MessageFormat.format(feedbackTemplate,
                 request.getVersion(),
                 request.getContact(),
                 request.getContent(),
