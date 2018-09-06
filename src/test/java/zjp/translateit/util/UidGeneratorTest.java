921fd4f839bacf9eb5fab540fcac4fe0f9015dfb
zunpiau <zunpiau@qq.com>
2018-09-06 17:03:24 +0800

    :recycle: Refactoring code.

--- src/test/java/zjp/translateit/util/UidGeneratorTest.java
+++ src/test/java/zjp/translateit/util/UidGeneratorTest.java
@@ -5,29 +5,24 @@ import org.junit.Test;
 
 import java.util.Collections;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class UidGeneratorTest {
 
     private static final int THREAD_COUNT = 64;
     private final UidGenerator generator = new UidGenerator();
     private final transient CountDownLatch exitLatch = new CountDownLatch(THREAD_COUNT);
-    private final transient CountDownLatch generatorLatch = new CountDownLatch(THREAD_COUNT);
+    private final transient CyclicBarrier cyclicBarrier = new CyclicBarrier(THREAD_COUNT);
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Test
     public void generate() throws InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
         Set<Long> set = Collections.newSetFromMap(new ConcurrentHashMap<>(THREAD_COUNT));
         for (int i = 0; i < THREAD_COUNT; i++) {
             executor.execute(() -> {
-                generatorLatch.countDown();
                 try {
-                    generatorLatch.await();
-                } catch (InterruptedException e) {
+                    cyclicBarrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
                     e.printStackTrace();
                 }
                 set.add(generator.generate());
