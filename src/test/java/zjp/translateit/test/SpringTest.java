705e1f30d998af818f2a11d28743be103959dfc6
zunpiau <zunpiau@qq.com>
2018-09-19 14:50:52 +0800

    :recycle: Refactoring code.

--- src/test/java/zjp/translateit/test/SpringTest.java
+++ src/test/java/zjp/translateit/test/SpringTest.java
@@ -1,14 +1,31 @@
 package zjp.translateit.test;
 
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import zjp.translateit.config.TestConfig;
+import zjp.translateit.config.RootConfig;
+import zjp.translateit.service.EmailService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TestConfig.class)
+@ContextConfiguration(classes = SpringTest.Config.class)
 @ActiveProfiles("test")
 public abstract class SpringTest {
 
+    @Configuration
+    @Import(RootConfig.class)
+    public static class Config {
+
+        @Bean
+        @Primary
+        public EmailService emailService() {
+            return Mockito.mock(EmailService.class);
+        }
+
+    }
 }
